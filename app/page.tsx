7b44d4ffd93508c1627ffce0acd5c7fa591a8841
abenaventehudtw-alt <abenaventehudtw@gmail.com>
2025-09-18 16:14:43 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import Carousel from '@/components/Carousel'
-
-export default function Home() {
-  return (
-    <section>
-      <div className="text-center max-w-3xl mx-auto mt-6 mb-6">
-        <p className="text-lg md:text-xl">
-          Este portavelas y portaincienso es más que un objeto decorativo.
-          Es un altar pequeño, creado para que recuerdes la luz que vive en ti.
-          Gracias por elegir con consciencia, belleza y alma.
-        </p>
-      </div>
-      <Carousel />
-    </section>
-  )
-}
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { CartProvider } from '../components/CartProvider'
